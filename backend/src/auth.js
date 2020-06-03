35261c3ef0f3ca3cdb6f51bfcb9ea12c78829a68
ExistenceisP4ain <57858720+ExistenceisP4ain@users.noreply.github.com>
2020-06-03 20:56:57 +0200

    Add files via upload

---
+++ backend/src/auth.js
@@ -0,0 +1,70 @@
+import mongo from 'mongodb';
+import connect from './db.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+// Kreiranje indeksa pri pokretanju aplikacije (ukoliko već ne postoji)
+(async () => {
+    let db = await connect();
+    db.collection('users').createIndex({ username: 1 }, { unique: true });
+})();
+export default {
+ async registerUser(userData) {
+ let db = await connect();
+ let result;
+    try {
+        let doc = {
+        username: userData.username,
+        // lozinku ćemo hashirati pomoću bcrypta
+        password: await bcrypt.hash(userData.password, 8),
+        name: userData.name,
+        tip: userData.tip
+        };
+    result = await db.collection('users').insertOne(doc);
+        } catch (e) {
+        if (e.name == 'MongoError') {
+        if (e.code == 11000) {
+        throw new Error('Username already exists');
+        }
+    }
+}
+if (result && result.insertedCount == 1) {
+return result.insertedId;
+} else {
+throw new Error('Cannot register user');
+}
+},
+async authenticateUser(username, password) {
+    let db = await connect();
+    let user = await db.collection('users').findOne({ username: username });
+    if (user && user.password && (await bcrypt.compare(password, user.password))) {
+        delete user.password; // ne želimo u tokenu, token se sprema na klijentu
+        let token = jwt.sign(user, process.env.JWT_SECRET, {
+        algorithm: 'HS512',
+        expiresIn: '1 week',
+    });
+    return {
+        token,
+        username: user.username,
+        tip:user.tip
+    };
+        } else {
+            throw new Error('Cannot authenticate');
+        }
+},
+verifyUser(token) {
+    try {
+        let authorization = req.headers.authorization.split(" ");
+        let type = authorization[0];
+        let token = authorization[1];
+  
+        if (type !== "Bearer") {
+          res.status(401).send();
+        } else {
+          req.jwt = jwt.verify(token, process.env.JWT_SECRET);
+          return next();
+        }
+      } catch (e) {
+        return res.status(401).send();
+      }
+},
+};
---
+++ frontend/src/services/index.js
@@ -0,0 +1,138 @@
+import axios from 'axios';
+import $router from '@/router'; // ovako importamo router izvan VUE componente
+
+// instanca axios-a za potrebe Fipugram backenda
+let Service = axios.create({
+    baseURL: 'http://localhost:3000/',
+    timeout: 1000,
+});
+
+// prije svakog poslanog requesta na backend izvrši:
+Service.interceptors.request.use((request) => {
+    try {
+    request.headers['Authorization'] = 'Bearer ' + Auth.getToken();
+    } catch (e) {
+    console.error(e);
+    }
+    return request;
+   });
+
+   // nakon svakog odgovora s backenda izvrši:
+Service.interceptors.response.use(
+    (response) => {
+        console.log('Interceptor', response);
+        return response;
+    },
+    (error) => {
+        if (error.response.status == 401) {
+            Auth.logout();
+            $router.go();
+        }
+        // console.error('Interceptor', error.response);
+    }
+);
+
+let Auth = {
+    // primamo user/pass, šaljemo upit na backend i ako dobijemo token
+    // spremimao ga u "localStorage" - JavaScript memoriju koja OSTAJE
+    // i nakon što zatvorimo preglednik ili osvježimo stranicu
+    async login(username, password) {
+    let response = await Service.post('/auth', {
+    username,
+    password,
+    });
+    let user = response.data;
+    // localStorage može sačuvati samo string, boolean ili number
+    // pa moramo pretvoriti objekt u string (JSON je string)
+    localStorage.setItem('user', JSON.stringify(user));
+    },
+    // logout znači brisanje tokena
+    logout() {
+    localStorage.removeItem('user');
+    },
+    // dohvat tokena
+    getToken() {
+    let user = Auth.getUser();
+    if (user && user.token) {
+    return user.token;
+    }
+    },
+    // dohvat spremljenog korisnika
+    getUser() {
+    return JSON.parse(localStorage.getItem('user'));
+    },
+    // provjera jesmo li autentificirani
+    authenticated() {
+    let user = Auth.getUser();
+    if (user && user.username) {
+    return true;
+    }
+    return false;
+    },
+    // state će nam služiti da ove varijable budu dostupne u vue.js
+ // radi se o "getter" funkcijama koje dopuštaju da ih čitamo
+ // kao da su varijable (slično kao Vue computed, ali na razini JS-a)
+ state: {
+    get user() {
+        return Auth.getUser();
+    },
+    get authenticated() {
+        return Auth.authenticated();
+    },
+    },
+   };
+
+// naš objekt za sve pozive koji se dotiču `Post`ova
+let Posts = {
+    Comments: {
+        async add(postId, comment) {
+            await Service.post(`/posts/${postId}/comments/`, comment);
+        },
+        async delete(postId, commentId) {
+            await Service.delete(`/posts/${postId}/comments/${commentId}`);
+        },
+    },
+    add(post) {
+        return Service.post('/posts', post);
+    },
+    async getOne(id) {
+        let response = await Service.get(`/posts/${id}`);
+
+        let doc = response.data;
+
+        return {
+            id: doc._id,
+            url: doc.source,
+            email: doc.createdBy,
+            title: doc.title,
+            posted_at: Number(doc.postedAt),
+            comments: (doc.comments || []).map((c) => {
+                c.id = c._id;
+                delete c._id;
+                return c;
+            }),
+        };
+    },
+    async getAll(searchTerm) {
+        let options = {};
+
+        if (searchTerm) {
+            options.params = {
+                _any: searchTerm,
+            };
+        }
+
+        let response = await Service.get('/posts', options);
+        return response.data.map((doc) => {
+            return {
+                id: doc._id,
+                url: doc.source,
+                email: doc.createdBy,
+                title: doc.title,
+                posted_at: Number(doc.postedAt),
+            };
+        });
+    },
+};
+
+export { Service, Posts, Auth }; // exportamo Service za ručne pozive ili Posts za metode.
